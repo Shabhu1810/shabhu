3e6529765faa7b7b97863972ab5b633ac5cb981b
Shabhu1810 <111517974+Shabhu1810@users.noreply.github.com>
2022-09-26 01:46:42 -0700

    Add files via upload

---
+++ PrintNumberWord1.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class PrintNumberWord1 {
+	public static void main(String[] args) {
+		int number = 5;
+		
+		switch(number) {
+		case 1:
+			System.out.println("one");
+			break;
+		case 2:
+			System.out.println("two");
+			break;
+		case 3:
+			System.out.println("three");
+			break;
+		case 4:
+			System.out.println("four");
+			break;
+		default:System.out.println("other");	
+		}
+	}
+}
