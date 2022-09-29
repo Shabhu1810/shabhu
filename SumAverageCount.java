43fad81ff82692a6586b7f9dc926261f1f59817a
Shabhu1810 <111517974+Shabhu1810@users.noreply.github.com>
2022-09-29 01:04:25 -0700

    Add files via upload

---
+++ SumAverageCount.java
@@ -0,0 +1,19 @@
+package Assignment;
+
+public class SumAverageCount {
+	public static void main(String[] args) {
+		int sum = 0;
+		double average = 0;
+		final int LOWERBOUND = 111;
+		final double UPPERBOUND = 8899;
+		int number = LOWERBOUND;
+		
+		for(int count = 1;count <= UPPERBOUND;++count) {
+			sum =+ number;
+			average = sum / number;
+			
+		}
+		System.out.println("The sum is " + sum);
+		System.out.println("The average is " + average);
+	}
+}
