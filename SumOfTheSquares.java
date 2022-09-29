572a4472274230ebc10a8488249934e19c78c3f4
Shabhu1810 <111517974+Shabhu1810@users.noreply.github.com>
2022-09-29 01:14:58 -0700

    Add files via upload

---
+++ SumOfTheSquares.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class SumOfTheSquares {
+	public static void main(String[] args) {
+		int sum = 1;
+		final int LOWERBOUND = 1;
+		final int UPPERBOUND = 100;
+		int number = LOWERBOUND;
+		
+		for(number = 1; number <= UPPERBOUND; number++) {
+			sum =+ (number * number);
+				System.out.println("The Sum Of Square From 1 to 100 is " + sum);
+			
+		}
+	}
+}
